3409e67a582932d91720ce96a01fd0509b37be87
khoa <khoa.soft.e@gmail.com>
2022-08-12 22:12:07 +0700

    Completed method

---
+++ script.rb
@@ -0,0 +1,18 @@
+require 'pry-byebug'
+def ceasar_cipher(string, shift)
+  chars = string.split("")
+  result = chars.map do |char|
+    if(char.match?(/[A-Za-z]/) == true)
+      base = char.ord < 91 ? 65 : 97
+      ascii_code = ((char.ord - base) + shift ) % 26 + base
+      ascii_code.chr
+    else
+      char
+    end
+  end
+  
+  result.join
+end
+
+result = ceasar_cipher("What a string!", -55)
+puts result
